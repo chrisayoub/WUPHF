046659027864b2f85ae9cceb34895b97b327a862
Ubuntu <ubuntu@ip-172-31-89-144.ec2.internal>
2017-10-22 22:54:10 +0000

    backend config updates

--- Backend/api.py
+++ Backend/api.py
@@ -219,4 +219,4 @@ api.add_resource(AddMemberToPack, '/pack/addMember')
 api.add_resource(RemoveMemberFromPack, '/pack/removeMember')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
