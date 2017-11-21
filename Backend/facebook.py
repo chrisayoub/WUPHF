aa615dda8f1bbb31d57950bc7b0c9de1f3efdaf7
Chris <chris@Chriss-iMac.local>
2017-11-20 23:20:29 -0600

    trying Facebook wuphf

--- Backend/facebook.py
+++ Backend/facebook.py
@@ -1,1 +1,8 @@
-# API to use: https://github.com/carpedm20/fbchat
+import facebook
+from db import FacebookAccount
+
+def makeWallPost(account, targetAccount, msg):
+	graph = facebook.GraphAPI(access_token=account.token, version="2.10")
+	graph.put_object(parent_object='me', connection_name='feed',
+                  message=msg, privacy=json.dumps(
+							        {'value' : 'CUSTOM', 'allow' : targetUser.fbId}))
