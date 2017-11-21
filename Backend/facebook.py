9d547381fdf4c5c36c525394bfa1f118153523c4
Chris <chris@Chriss-iMac.local>
2017-11-21 03:03:36 -0600

    FB API

--- Backend/facebook.py
+++
@@ -1,8 +0,0 @@
-import facebook
-from db import FacebookAccount
-
-def makeWallPost(account, targetAccount, msg):
-	graph = facebook.GraphAPI(access_token=account.token, version="2.10")
-	graph.put_object(parent_object='me', connection_name='feed',
-                  message=msg, privacy=json.dumps(
-							        {'value' : 'CUSTOM', 'allow' : targetUser.fbId}))
---
+++ Backend/facebookApi.py
@@ -0,0 +1,9 @@
+from db import FacebookAccount
+import json
+import requests
+
+def makeWallPost(account, targetAccount, msg):
+	payload =  {'privacy' : '{\'value\': \'SELF\'}', 
+				'message' : msg, 'tags' : str(targetAccount.fbId)}
+	requests.post("https://graph.facebook.com/v2.11/me/feed?access_token=" + account.token, 
+		data=payload)
