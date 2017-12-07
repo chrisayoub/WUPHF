0c4612b0d8f723456f59e1798b295c75949958b7
Chris <chris@Chriss-iMac.local>
2017-12-07 01:14:53 -0600

    keyboard dismiss

--- WUPHF/View Controllers/EditProfileViewController.swift
+++ WUPHF/View Controllers/EditProfileViewController.swift
@@ -95,4 +95,7 @@ class EditProfileViewController: UIViewController {
             }
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
